48c87441e372df94f59b757a73c36ded32ec2aac
Muhammad Sandi <sandimuhammad7@gmail.com>
2022-10-21 16:01:45 +0800

    fix navbar dan ukuran modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,8 +69,8 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
+  // var dots = document.getElementsByClassName("demo");
+  // var captionText = document.getElementById("caption");
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -80,10 +80,10 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+  // for (i = 0; i < dots.length; i++) {
+  //   dots[i].className = dots[i].className.replace(" active", "");
+  // }
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
+  // dots[slideIndex - 1].className += " active";
+  // captionText.innerHTML = dots[slideIndex - 1].alt;
 }
